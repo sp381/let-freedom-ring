f05b8e674f94a41f0e53b8442d359b6557949fc9
sp381 <sarah.powell381@gmail.com>
2022-06-28 13:05:56 -0700

    initial commit

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+//import Button from 'react-bootstrap/Button';
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
