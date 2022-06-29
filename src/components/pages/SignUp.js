8171c838dbd5c09ef95ab7b43458fad08490be63
sp381 <sarah.powell381@gmail.com>
2022-06-29 11:15:26 -0700

    fixing bugs

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -55,12 +55,13 @@ display: submitted ? '' : 'none',
 // Showing error message if error is true
 const errorMessage = () => {
 return (
+  
 <div
 className="error"
 style={{
 display: error ? '' : 'none',
 }}>
-<h1>Please enter all the fields</h1>
+<h2>Please enter all the fields</h2>
 </div>
 );
 };
@@ -79,22 +80,45 @@ return (
 
 <form>
 {/* Labels and inputs for form data */}
+<br />
 <label className="label">Name</label>
 <input onChange={handleName} className="input"
 value={name} type="text" />
-
+<br />
+<br />
 <label className="label">Email</label>
 <input onChange={handleEmail} className="input"
 value={email} type="email" />
-
+<br />
+<br />
 <label className="label">Password</label>
 <input onChange={handlePassword} className="input"
 value={password} type="password" />
-
+<br />
+<br />
 <button onClick={handleSubmit} className="btn" type="submit">
-Submit
+Let's Eat!
 </button>
 </form>
+<br />
+{/* Contact Form */}
+{/* <br />
+<br />
+<form>
+  <label>Contact</label>
+  <br />
+  <br />
+  <label className="label">Name</label> <br />
+  <input onChange={handleName} className="input"
+  value={name} type="text" />
+  <br />
+  <br />
+
+  <button onClick={handleSubmit} className="btn" type="submit">
+  Submit
+  </button>
+</form> */}
 </div>
 );
+
 }
