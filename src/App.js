ff7bc492b647809af59e51fd8db1c1911d88a519
carternadain <cnadain@gmail.com>
2022-07-02 17:08:23 -0700

    update to app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import PortfolioContainer from "./components/PortfolioContainer";
 import './App.css';
 // import CountdownTimer from './Components/CountdownTimer/CountdownTimer';
 
-
 const App = () => <PortfolioContainer />;
 
+function App() {
+    return (
+      <div className="App">
+       <CountdownTimer
+       countdownTimestampMs={1659983662000}/>
+      </div>
+    );
+  }
 export default App;
