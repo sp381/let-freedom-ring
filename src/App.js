00bb7fdf523ddff5eb46c1ff786e57f92595c895
carternadain <cnadain@gmail.com>
2022-07-02 17:02:54 -0700

    add app.js connections for timer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
+import './App.css';
+// import CountdownTimer from './Components/CountdownTimer/CountdownTimer';
+
 
 const App = () => <PortfolioContainer />;
 
