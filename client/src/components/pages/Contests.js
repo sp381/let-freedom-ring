2e2c7985c02ec5997d0e1670eb0ead97d7460b69
Oscar Hernandez <oscarhernandezmex@gmail.com>
2022-07-04 17:32:43 -0700

    Contests files updated to 6

--- client/src/components/pages/Contests.js
+++ client/src/components/pages/Contests.js
@@ -1,15 +1,16 @@
 import React, { Component, useState } from "react";
-// import PropTypes from "prop-types";
 import Logo from "../../assets/get in my belly.gif";
 import Nathans from "../../assets/Nathans.webp";
 import Countdown from "react-countdown";
-import contests from "../../utils/contests";
 import './Contests.css';
+// import { SlackSelector } from 'react-reactions';
+// import contests from "../../utils/contests";
+// import PropTypes from "prop-types";
+
+
 
 export default function Contest(props) {
-  // use state hooks here as needed
   const [contest, setContest] = useState("hot dog contest");
-  // const [, setContest] = useState("");
 
   return (
     <div className="Contests">
@@ -25,6 +26,70 @@ export default function Contest(props) {
           <li>
             <a href="http://www.twitter.com/eatingcontest">Nathans winner</a>
           </li>
+          <Countdown date={Date.parse("29 sep 2025 00:4:00 GMT")} />
+          
+
+          
+        </ul>
+
+        <img src={Nathans} alt="nathans" style={{ width: "150px" }} />
+      </div>
+      <div>
+        <p>Nathans Famous fourth of July Hot Dog Eating Contest</p>
+        <ul>
+          <li>
+            <a href="http://www.twitter.com/eatingcontest">Nathans winner</a>
+          </li>
+          <Countdown date={Date.parse("29 sep 2025 00:4:00 GMT")} />
+          
+        </ul>
+
+        <img src={Nathans} alt="nathans" style={{ width: "150px" }} />
+      </div>
+      <div>
+        <p>Nathans Famous fourth of July Hot Dog Eating Contest</p>
+        <ul>
+          <li>
+            <a href="http://www.twitter.com/eatingcontest">Nathans winner</a>
+          </li>
+          <Countdown date={Date.parse("29 sep 2025 00:4:00 GMT")} />
+          
+        </ul>
+
+        <img src={Nathans} alt="nathans" style={{ width: "150px" }} />
+      </div>
+      <div>
+        <p>Nathans Famous fourth of July Hot Dog Eating Contest</p>
+        <ul>
+          <li>
+            <a href="http://www.twitter.com/eatingcontest">Nathans winner</a>
+          </li>
+          <Countdown date={Date.parse("29 sep 2025 00:4:00 GMT")} />
+          
+        </ul>
+
+        <img src={Nathans} alt="nathans" style={{ width: "150px" }} />
+      </div>
+      <div>
+        <p>Nathans Famous fourth of July Hot Dog Eating Contest</p>
+        <ul>
+          <li>
+            <a href="http://www.twitter.com/eatingcontest">Nathans winner</a>
+          </li>
+          <Countdown date={Date.parse("29 sep 2025 00:4:00 GMT")} />
+          
+        </ul>
+
+        <img src={Nathans} alt="nathans" style={{ width: "150px" }} />
+      </div>
+      <div>
+        <p>Nathans Famous fourth of July Hot Dog Eating Contest</p>
+        <ul>
+          <li>
+            <a href="http://www.twitter.com/eatingcontest">Nathans winner</a>
+          </li>
+          <Countdown date={Date.parse("29 sep 2025 00:4:00 GMT")} />
+          
         </ul>
 
         <img src={Nathans} alt="nathans" style={{ width: "150px" }} />
@@ -35,15 +100,17 @@ export default function Contest(props) {
         // onClick={this.state.contestListClick}
       ></div>
       <Countdown date={Date.parse("29 sep 2025 00:4:00 GMT")} />
+      
 
-      <div>
+      {/* <div>
         {contests.map((contest, i) => (
           <div key={`contest${i}`}>
             <h2 className="contest-heading">{contest.name}</h2>
             <Countdown date={Date.parse(contest.time)} />
+            
           </div>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
