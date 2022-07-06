f7cbf3a5134563f1f11d02d6d5b6d7067dfca23b
sp381 <sarah.powell381@gmail.com>
2022-07-06 15:05:03 -0500

    debug

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-//Bootstrap CSS
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import './index.css';
-import App from './App';
 
+import "./index.css";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
